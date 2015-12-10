224c7d719f9f57607a3ea5f126b709ace0932eba
Alice Maz <alice@alicemaz.com>
2015-12-10 15:45:25 -0500

    Fix globbing bug in Makefile

--- src/head.js
+++ src/head.js
@@ -4,3 +4,29 @@ const LIM = 500;
 const TZ = (new Date()).getTimezoneOffset() * 60 * 1000;
 
 let socket; 
+
+const ajax = (method,target) => {
+    return new Promise((Y,N) => {
+        let req = new XMLHttpRequest();
+
+        req.open(method, target, true);
+
+        req.onreadystatechange = () => {
+            if(req.readyState == 4) { 
+                if(req.status >= 200 && req.status < 400) { 
+                    try {
+                        let res = JSON.parse(req.response);
+                        Y(res);
+                    } catch(err) {
+                        N(err);
+                    }
+                }
+                else {
+                    N(new Error(`${req.url} failed: ${req.status} ${req.statusText}`));
+                }
+            }
+        };
+
+        req.send();
+    });
+};
